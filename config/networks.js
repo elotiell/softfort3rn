42fc82f2c7685a3b4f5f718957ddf06e4c559539
elotiell <120194751+elotiell@users.noreply.github.com>
2024-09-09 13:11:49 +0200

    Update networks.js

--- config/networks.js
+++ config/networks.js
@@ -1,14 +1,14 @@
 module.exports = {
     arbitrum: {
         rpcUrl: 'https://arbitrum-sepolia.blockpi.network/v1/rpc/public',
-        chainId: 421611  // Chain ID для Arbitrum Sepolia
+        chainId: 421611  
     },
     optimism: {
         rpcUrl: 'https://sepolia.optimism.io',
-        chainId: 420  // Chain ID для Optimism Sepolia
+        chainId: 420  
     },
     wallet: {
-        address: process.env.WALLET_ADDRESS,  // Адрес кошелька
-        privateKey: process.env.PRIVATE_KEY   // Приватный ключ
+        address: process.env.WALLET_ADDRESS,  
+        privateKey: process.env.PRIVATE_KEY   
     }
 };
