db55282280a4a5336a8c5f34b31b437f96c69df1
elotiell <120194751+elotiell@users.noreply.github.com>
2024-09-09 13:12:51 +0200

    Delete src/src directory

--- src/src/balance.js
+++
@@ -1,6 +0,0 @@
-async function checkOptimismBalance(web3, address) {
-    const balance = await web3.eth.getBalance(address);
-    console.log('Баланс на Optimism Sepolia:', web3.utils.fromWei(balance, 'ether'));
-}
-
-module.exports = { checkOptimismBalance };
