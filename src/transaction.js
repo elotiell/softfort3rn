620777b8f1aac3fa48d62d9bd74433a9bf5a1b7c
elotiell <120194751+elotiell@users.noreply.github.com>
2024-09-09 13:05:24 +0200

    Create transaction.js

---
+++ src/transaction.js
@@ -0,0 +1,16 @@
+const Web3 = require('web3');
+const { wallet } = require('../config/networks');
+
+// Подписываем и отправляем транзакцию
+async function sendSignedTransaction(web3, tx) {
+    const signedTx = await web3.eth.accounts.signTransaction(tx, wallet.privateKey);
+    return web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+        .on('receipt', (receipt) => {
+            console.log('Транзакция подтверждена:', receipt);
+        })
+        .on('error', (error) => {
+            console.error('Ошибка при отправке транзакции:', error);
+        });
+}
+
+module.exports = { sendSignedTransaction };
